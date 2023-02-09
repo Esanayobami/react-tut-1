e085865f8d21591c61acce262d6862dd1c35e5d7
dicey <esanelijah@icloud.com>
2023-02-08 17:44:48 -0800

    some extra bugs

--- reactjs-tut/src/App.js
+++ reactjs-tut/src/App.js
@@ -3,50 +3,47 @@ import Content from "./Content";
 import Header from "./Header";
 import AddItem from "./AddItem";
 import Footer from "./Footer";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import SearchItems from "./SearchItems";
 
 function App() {
 
         // an empty array was created to prevent error
-        const [items , setItems] = useState(
-        JSON.parse(localStorage.getItem("shoppinglist")) || []
-          
-      );
-      const [newItem , setNewItem] = useState(" ");
-      const [search , setSearch] = useState(" ");
+      const [items , setItems] = useState(JSON.parse(localStorage.getItem("shoppingList")) || [] );
+      const [newItem , setNewItem] = useState("");
+      const [search , setSearch] = useState("");
 
-      const setAndSaveItems = (newItems) => {
-        setItems(newItems);
-        localStorage.setItem("shoppinglist" , JSON.stringify(newItems));
-      }
+    useEffect(() =>{
+      localStorage.setItem("shoppingList" , JSON.stringify(items));
+    } ,[items])
+
+  
       
       const addItem = (item) => {
         const id = items.length ? items[items.length - 1].id + 1 : 1;
         const myNewItem = { id, checked: false, item };
         const listItems = [...items, myNewItem];
-        setAndSaveItems(listItems);
+        setItems(listItems);
       }
     
     
 
       const handleCheck = (id) => {
         const listItems = items.map((item) => item.id === id ? {...item ,checked : !item.checked} : item );
-        setAndSaveItems(listItems);
+        setItems(listItems);
       }
 
       const handleDelete = (id) =>{
         const listItems = items.filter((item) => item.id !== id);
-        setAndSaveItems(listItems);
+        setItems(listItems);
       }
       const handleSubmit = (e) =>{
         e.preventDefault();
         if(!newItem.trim()) return;
-        addItem(newItem);
+        addItem(newItem.trim());
         setNewItem("");
-
-
       }
+
   return (
     <div className="App">
       <Header title="Groceries" />
@@ -61,12 +58,26 @@ function App() {
         search={search}
         setSearch={setSearch}
        />
-     {/* buggyy */}
-     <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+
+{items.length ? (
+  <Content
+    items={items.filter((item) => (item.item.toLowerCase()).includes(search.toLowerCase()))}
+    handleCheck={handleCheck}
+    handleDelete={handleDelete}
+  />
+) : (
+  <p>No items to display</p>
+)}
+
+
+
+
+
+
+
+
+
+
 
       <Footer length={items.length} />
 
